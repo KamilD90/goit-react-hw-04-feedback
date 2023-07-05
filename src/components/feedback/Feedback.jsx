d8c01d0974266f50097bd9cdc59ae9e36039b4be
KamilD90 <117952413+KamilD90@users.noreply.github.com>
2023-07-05 20:17:21 +0200

    works

--- src/components/feedback/Feedback.jsx
+++ src/components/feedback/Feedback.jsx
@@ -1,57 +1,34 @@
 import css from './Feedback.module.css';
-import { Component } from "react";
+import { useState , useEffect} from "react";
 
- export class Feedback extends Component{
+export const Feedback = () => {
+    const [positive, setPositive] = useState(0);
+    const [neutral, setNeutral] = useState(0);
+    const [negative, setNegative]= useState(0);
+    const [total, setTotal]= useState (0);
+    const [positivePercentage, setPositivePercentage]= useState(0);
+
+const handlePositive= () => setPositive(positive+1);
+
+const handleNeutral= () => setNeutral(neutral+1);
+
+const handleNegative=()=> setNegative(negative+1);
 
 
-    constructor (props) {
-        super(props);
-        this.state={
-            good: 0,
-            neutral: 0, 
-            bad: 0, 
-            // total: 0,
-        }
-    }
 
-    handlePositive = ()=> {
-        this.setState(prevState => ({
-            good: prevState.good + 1
-        }));
-    }
-    
+useEffect(()=> {
+   setTotal( positive + neutral + negative);
+   setPositivePercentage((positive/total)*100);
+}, [positive, neutral, negative, total]);
 
-    handleNeutral =() => {
-        this.setState(prevState => ({
-            neutral: prevState.neutral + 1
-        }));
-    }
 
-    handleNegative = () => {
-        this.setState( prevState => ({
-            bad: prevState.bad + 1
-        }));
-    }
-
-    countTotalFeedback = () => {
-     const { good, neutral, bad } = this.state;
-     // lub  :  const total = this.state.good + this.state.neutral + this.state.bad
-     const total = good+ bad + neutral;
-     this.setState({total : total});
-    }
-
-    render () {
-        const {good, neutral, bad} = this.state
-        const total = good + neutral + bad; 
-        const positivePercentage= (good/total) *100 ; 
-
-        return (
-            <div className={css.wrapper}>
+return (
+    <div className={css.wrapper}>
                 <h2 className={css.title}>Please leave your feedback</h2>
                 <div className={css.btn__box}>
-                    <button type="button" onClick={this.handlePositive} className={[ css.btn, css.btn__positive ].join(' ')}>Positive</button>
-                    <button type="button" onClick={this.handleNeutral} className={[css.btn, css.btn__neutral].join(' ')}>Neutral</button>
-                    <button type="button" className={[css.btn, css.btn__negative].join(' ')} onClick={this.handleNegative}>Negative</button>
+                    <button type="button" onClick={handlePositive} className={[ css.btn, css.btn__positive ].join(' ')}>Positive</button>
+                    <button type="button" onClick={handleNeutral} className={[css.btn, css.btn__neutral].join(' ')}>Neutral</button>
+                    <button type="button" onClick={handleNegative} className={[css.btn, css.btn__negative].join(' ')} >Negative</button>
                 </div>
              
                 <h3 className={css.subTitle}>Statistic</h3>
@@ -63,7 +40,7 @@ import { Component } from "react";
                     <span className={css.statList__item__name}>
                         Positive:
                     </span>
-                    <span className={css.statList__item__value}>{good}</span>
+                    <span className={css.statList__item__value}>{positive}</span>
                     </li>
                     <li className={css.statList_item}>
                     <span className={css.statList__item__name}>
@@ -75,7 +52,7 @@ import { Component } from "react";
                     <span className={css.statList__item__name}>
                         Negative:
                     </span>
-                    <span className={css.statList__item__value}>{bad}</span>
+                    <span className={css.statList__item__value}>{negative}</span>
                     </li>
                     <li className={css.statList_item}>
                     <span className={css.statList__item__name}>
@@ -94,9 +71,7 @@ import { Component } from "react";
 
       </div>
         );
-    }
 
 }
 
 export default Feedback;
-
